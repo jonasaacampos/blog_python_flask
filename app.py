815fccf3350a2cf1632f74ee75cd175a19c9f78e
jaac <38704271+jonasaacampos@users.noreply.github.com>
2022-08-04 17:41:53 -0300

    validação de login/cadastro e envio de mensagem de sucesso (backend)

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, flash, redirect
 from credentials import get_secret
 import forms
 
@@ -29,6 +29,15 @@ def usuarios():
 def login():
     form_login = forms.FormLogin()
     form_criar_conta = forms.FormCriarConta()
+
+    if form_login.validate_on_submit() and 'btn_submit_login' in request.form:
+        flash(f'Seja bem vindo {form_login.email.data}', 'alert-success')
+        return redirect(url_for('home'))
+
+    if form_criar_conta.validate_on_submit() and 'btn_submit_criar_conta' in request.form:
+        flash(f'Conta criada com sucesso para {form_criar_conta.email.data}', 'alert-success')
+        return redirect(url_for('home'))
+
     return render_template('login.html', form_login=form_login, form_criar_conta=form_criar_conta)
 
 
--- forms.py
+++ forms.py
@@ -10,12 +10,12 @@ class FormCriarConta(FlaskForm):
     username = StringField('Nome de usuário:', validators=[DataRequired()])
     email = StringField('e-mail:', validators=[DataRequired(), Email()])
     senha = PasswordField('senha:', validators=[DataRequired(), Length(6, 20)])
-    senha_confimacao = StringField('digite novamamente sua senha:', validators=[EqualTo('senha')])
-    btn_submit = SubmitField('Criar Conta')
+    senha_confimacao = PasswordField('digite novamamente sua senha:', validators=[EqualTo('senha')])
+    btn_submit_criar_conta = SubmitField('Criar Conta')
 
 
 class FormLogin(FlaskForm):
-    email = StringField('e-mail:', validators=[DataRequired()])
+    email = StringField('e-mail:', validators=[DataRequired(), Email()])
     senha = PasswordField('senha:', validators=[DataRequired(), Length(6, 20)])
     check_lembrar_dados = BooleanField('Lembrar dados de acesso')
     btn_submit_login = SubmitField('Login')
