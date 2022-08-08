045e5b716f0a2b736311c9ff55c7be69daec6071
jaac <38704271+jonasaacampos@users.noreply.github.com>
2022-08-08 15:08:14 -0300

    validação de email único e personlização da excessão

--- forms.py
+++ forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from models import Usuario
 
 
 # pip install email-validator
@@ -12,6 +13,13 @@ class FormCriarConta(FlaskForm):
     senha = PasswordField('senha:', validators=[DataRequired(), Length(6, 20)])
     senha_confimacao = PasswordField('digite novamamente sua senha:', validators=[EqualTo('senha')])
     btn_submit_criar_conta = SubmitField('Criar Conta')
+
+    def validate_email(self, email):
+        user_mail = Usuario.query.filter_by(email=email.data).first()
+        if user_mail:
+            raise ValidationError('E-mail já cadastrado. Utilize outro email ou faça login para continuar')
+
+
 
 
 class FormLogin(FlaskForm):
